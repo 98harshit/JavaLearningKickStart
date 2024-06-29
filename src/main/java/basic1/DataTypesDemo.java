ad9764fe16400a11a45f592d32f281cd2543806e
harshit98 <harshitgiri1998@gmail.com>
2024-06-29 15:46:44 +0530

    data types demo

---
+++ src/main/java/basic1/DataTypesDemo.java
@@ -0,0 +1,25 @@
+package basic1;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		
+		byte bt = -128;
+		short sht = 32767;
+		int myInt = 764765;
+		long lng = 765737l;
+		 
+		float myFloat = 1.455667f;
+		double myDouble = 1.455656d;
+		
+		boolean bool = false;
+		char c = 'c';
+		
+		DataTypesDemo dt = new DataTypesDemo();
+		String str = new String();
+		
+		System.out.println(bt);
+
+	}
+
+}
