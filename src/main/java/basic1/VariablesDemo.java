bc51ec2fdc3f55ef502afb8c4d679d3c75dcfda1
harshit98 <harshitgiri1998@gmail.com>
2024-07-02 23:43:11 +0530

    Variables demo

---
+++ src/main/java/basic1/VariablesDemo.java
@@ -0,0 +1,21 @@
+package basic1;
+
+public class VariablesDemo {
+	
+	int i=50; //instance variable
+
+	public static void main(String[] args) {
+		
+		//type variableName = value;
+		
+		int myVariable = 10; //local variable
+		System.out.println(myVariable);
+
+	}
+	
+	public void myMethod()
+	{
+		int j = 20; //local variable
+	}
+
+}
