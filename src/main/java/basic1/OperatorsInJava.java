0357a793b33ca4cbe13f266cf1132480b2691ee9
harshit98 <harshitgiri1998@gmail.com>
2024-07-03 23:57:24 +0530

    operators in java

---
+++ src/main/java/basic1/OperatorsInJava.java
@@ -0,0 +1,26 @@
+package basic1;
+
+public class OperatorsInJava {
+
+	public static void main(String[] args) {
+		
+		//Arithmetic operator
+		int myNumber = 20;
+		String str = "String demo";
+		
+		int number1 = 10;
+		int number2 = 30;
+		
+		if(number1!=number2)
+		{
+			System.out.println("Num1 is not equal to Num2");
+		}
+		else
+		{
+			System.out.println("Num1 is equal to num2");
+		}
+		
+
+	}
+
+}
