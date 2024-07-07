a86c65f5e97789434211e7de2c421b35af6389fc
harshit98 <harshitgiri1998@gmail.com>
2024-07-07 23:07:59 +0530

    Ternary operator demo

---
+++ src/main/java/basic1/TernaryOperatorDemo.java
@@ -0,0 +1,18 @@
+package basic1;
+
+public class TernaryOperatorDemo {
+
+	public static void main(String[] args) {
+
+
+		//variable = (Condition) ? True Expression : False Expression
+		
+		int num1=20;
+		int num2=30;
+		
+		String result = (num1<num2) ? "Num1<Num2" : "Num1>Num2";
+		System.out.println(result);
+
+	}
+
+}
