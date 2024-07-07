c2ef0c5c77e1c6be9be084178fe3142a1da6ab30
harshit98 <harshitgiri1998@gmail.com>
2024-07-07 23:08:54 +0530

    while loop demo

---
+++ src/main/java/basic1/WhileLoopDemo.java
@@ -0,0 +1,18 @@
+package basic1;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		System.out.println("Before while loop");
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("After while loop");
+
+	}
+
+}
