fbed86cbae8b527b790fb5782fb17d003bf68b11
harshit98 <harshitgiri1998@gmail.com>
2024-07-08 00:27:05 +0530

    for loop demo

---
+++ src/main/java/basic1/ForLoopDemo.java
@@ -0,0 +1,20 @@
+package basic1;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+		
+		//WhileLoop - when you don't know the number of iterations
+		
+		//DoWhileLoop - when you want to execute the code at least once irrespective of what ever the condition is
+		
+		//ForLoop - you know how many iterations you want to run
+
+	}
+
+}
