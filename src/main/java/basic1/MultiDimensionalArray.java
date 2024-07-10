bfc30ed44b5825e1cfade800d138354180f248b7
harshit98 <harshitgiri1998@gmail.com>
2024-07-10 23:59:36 +0530

    multidimensional array

---
+++ src/main/java/basic1/MultiDimensionalArray.java
@@ -0,0 +1,20 @@
+package basic1;
+
+public class MultiDimensionalArray {
+
+	public static void main(String[] args) {
+
+
+		int[][] arr1 = {{1,2,3},{4,5,6}};
+		
+		for(int i=0;i<arr1.length;i++)
+		{
+			for(int j=0;j<arr1[i].length;j++)
+			{
+				System.out.println(arr1[i][j]);
+			}
+		}
+
+	}
+
+}
