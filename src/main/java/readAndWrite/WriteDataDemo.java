3d4812e29d46dcb87a4ef0397e470ef2e0642b05
harshit98 <harshitgiri1998@gmail.com>
2024-09-22 13:06:53 +0530

    write new file

--- src/main/java/readAndWrite/WriteDataDemo.java
+++ src/main/java/readAndWrite/WriteDataDemo.java
@@ -11,7 +11,7 @@ public static void main(String[] args) throws IOException {
 		
 		//Step 1 - create an object of fileWriter class
 		//FileWriter fr = new FileWriter("C:\\Users\\LENOVO\\eclipse-workspace\\new-workspace\\JavaLearningKickStart\\src\\main\\java\\readAndWrite\\TestData.properties");
-		FileOutputStream fis = new FileOutputStream(System.getProperty("user.dir")+"\\\\src\\\\main\\\\java\\\\readAndWrite\\\\TestData.properties");
+		FileOutputStream fis = new FileOutputStream(System.getProperty("user.dir")+"\\\\src\\\\main\\\\java\\\\readAndWrite\\\\TestData1.properties");
 		
 		//Step 2 - create an object of properties class
 		Properties p = new Properties();
