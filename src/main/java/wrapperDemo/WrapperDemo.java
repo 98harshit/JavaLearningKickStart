e53bfd13ada7580aa301c8c973279198ce3c4343
harshit98 <harshitgiri1998@gmail.com>
2024-09-22 23:01:54 +0530

    wrapper class demo

---
+++ src/main/java/wrapperDemo/WrapperDemo.java
@@ -0,0 +1,15 @@
+package wrapperDemo;
+
+public class WrapperDemo {
+	
+	public static void main(String[] args) {
+		
+		int i = 10;
+		Integer int1 = new Integer(i); //wrapping
+		System.out.println(int1);
+		
+		int j = int1; //Unwrapping
+		System.out.println(j);
+	}
+
+}
