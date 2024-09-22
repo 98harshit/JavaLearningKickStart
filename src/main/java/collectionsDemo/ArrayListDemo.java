f1566554765dc2617187c31a2780950863c00d85
harshit98 <harshitgiri1998@gmail.com>
2024-09-22 23:24:59 +0530

    array list demo

---
+++ src/main/java/collectionsDemo/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package collectionsDemo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add("String");
+		al.add(true);
+		System.out.println(al.size());
+		System.out.println(al);
+	}
+
+}
